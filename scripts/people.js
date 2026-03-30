cb440ee1d5a2440f4423231053ff7fd2240bc87e
Ilegubo <ilegubo@gmail.com>
2026-03-30 18:08:16 +0000

    Remove them

--- scripts/people.js
+++ scripts/people.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAK8a2GI2F9lqaw-HafOH537Nc7tl0cKDA",
-  authDomain: "confirm-2026.firebaseapp.com",
-  projectId: "confirm-2026",
-  storageBucket: "confirm-2026.firebasestorage.app",
-  messagingSenderId: "969420589666",
-  appId: "1:969420589666:web:57a9436869478447111f3b",
-  measurementId: "G-SWD2QVW3YJ"
+  
 };
 
 // Initialize Firebase
