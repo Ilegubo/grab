92b37aee1e16f319cb7bf5e3b83a970cbc2c8641
Ilegubo <ilegubo@gmail.com>
2026-03-30 18:12:13 +0000

    Remove them

--- scripts/people.js
+++ scripts/people.js
@@ -1,21 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const FORMSPREE_URL = "https://formspree.io/f/your-id-here"; // <--- CHANGE THIS
-
 document.getElementById('verifyBtn').addEventListener('click', async () => {
     const status = document.getElementById('status');
     const email = document.getElementById('email').value;
